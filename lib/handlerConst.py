73e7a7741593ee46edfdc12e64946c0013ba8131
Manousos Linardakis <it22064@hua.gr>
2023-01-22 00:18:27 +0200

    test loan handler compile

--- lib/handlerConst.py
+++ lib/handlerConst.py
@@ -277,9 +277,137 @@ END h_add_book;
 END;
 """
 
+# loan handler object:
+upd_deadline_date = """
+CREATE OR REPLACE PROCEDURE update_deadline_date ( isbn_loaned_book IN NUMBER, new_deadline DATE)
+IS
+    cursor c1 is
+    SELECT * from Loans
+    where isbn=isbn_loaned_book;
+BEGIN  
+    FOR book_rec in c1
+    LOOP      
+        IF is_loan ( isbn_loaned_book)=1 then
+            if book_rec.loan_date < new_deadline then
+                UPDATE Loans 
+                SET loan_deadline= new_deadline
+                WHERE isbn=isbn_loaned_book AND return_date IS NULL;
+                dbms_output.put_line('Deadline date updated.');
+            else
+                dbms_output.put_line('Did not update deadline (deadline should be after loan date).');                
+            end if;
+        ELSE
+            dbms_output.put_line('The book is already returned.');
+        END IF;
+    END LOOP;
+    if book_already_exists(isbn_loaned_book) != 1 then
+        dbms_output.put_line('Could not find requested book.');        
+    end if;
+END;
+"""
+
+get_loans_days = """
+CREATE OR REPLACE Function get_loans_days ( isbn IN number ) RETURN number
+IS
+    res number;
+   cursor c1 is
+     SELECT isbn, loan_date, return_date
+     FROM loans;
+BEGIN
+    res := is_loan(isbn);
+    if res = 1 then
+        return 0;
+    end if;
+   FOR assign_to_rec in c1
+   LOOP
+      IF isbn = assign_to_rec.isbn then
+            if assign_to_rec.return_date is null then
+                return to_date(CURRENT_DATE) - assign_to_rec.loan_date; -- book not returned yet.
+            else
+                return -2;  -- book is returned.
+            end if;
+      end if;
+   END LOOP;
+   return -1;
+END;
+"""
+
+get_fine = """
+CREATE OR REPLACE Function get_fine ( isbn IN number, daily_fine_cost IN number ) RETURN number
+IS
+    res number;
+   fine_days number(6);
+   calc_days number(6);
+   cursor c1 is
+     SELECT isbn, loan_deadline, return_date
+     FROM loans;
+BEGIN
+   FOR assign_to_rec in c1
+   LOOP
+      IF isbn = assign_to_rec.isbn then -- book is loaned:
+            --if assign_to_rec.return_date = null then    -- book has not been returned:
+            IF assign_to_rec.return_date IS NULL then
+                fine_days := to_date(CURRENT_DATE) - assign_to_rec.loan_deadline;
+                return daily_fine_cost*fine_days;
+            end if;
+            calc_days := assign_to_rec.return_date - assign_to_rec.loan_deadline;
+            if calc_days <= 0 then
+                return 0;
+            else
+                return daily_fine_cost*calc_days;
+            end if;
+      end if;
+   END LOOP;
+   return -1;
+END;
+"""
+
+loan_handler_obj = """
+CREATE OR REPLACE TYPE loans_handler_obj AS OBJECT
+(
+id number(3),
+-- procedures:
+MEMBER PROCEDURE h_return_book(isbn_loaned_book NUMBER), 
+MEMBER PROCEDURE h_update_deadline_date(isbn_loaned_book NUMBER, new_deadline DATE),
+
+-- functions:
+MEMBER Function h_is_loan(isbn NUMBER) RETURN NUMBER, 
+MEMBER Function h_get_loans_days ( isbn number )RETURN NUMBER, 
+MEMBER Function h_get_fine ( isbn number, daily_fine_cost number ) RETURN NUMBER
+);
+"""
+
+loan_handler_obj_body = """
+CREATE OR REPLACE TYPE BODY loans_handler_obj AS
+-- functions:
+MEMBER Function h_is_loan(isbn NUMBER) RETURN NUMBER IS
+BEGIN
+    return is_loan(isbn);
+END h_is_loan;
+MEMBER Function h_get_loans_days ( isbn number )RETURN NUMBER IS 
+BEGIN 
+    return get_loans_days(isbn);
+END h_get_loans_days;
+MEMBER Function h_get_fine ( isbn number, daily_fine_cost number ) RETURN NUMBER IS
+BEGIN 
+    return get_fine(isbn, daily_fine_cost);
+END h_get_fine;
+-- procedures:
+MEMBER PROCEDURE h_return_book(isbn_loaned_book NUMBER) IS
+BEGIN 
+    return_book(isbn_loaned_book);
+END h_return_book;
+MEMBER PROCEDURE h_update_deadline_date(isbn_loaned_book NUMBER, new_deadline DATE) IS 
+BEGIN 
+    update_deadline_date(isbn_loaned_book, new_deadline );
+END h_update_deadline_date;
+
+END;
+"""
+
 class Compile():
     def member_handler_obj(cursor):
-        '''Compiles required functions and procedures for member handler object'''
+        '''Compiles required functions and procedures for member handler object.'''
         cursor.execute(book_already_exists)
         cursor.execute(is_loan)
         cursor.execute(lend_book_member)
@@ -287,7 +415,7 @@ class Compile():
         cursor.execute(member_handler_obj_body)
 
     def book_handler_obj(cursor):
-        '''Compiles required functions and procedures for book handler object'''
+        '''Compiles required functions and procedures for book handler object.'''
         cursor.execute(book_already_exists)
         cursor.execute(is_loan)
         cursor.execute(has_category)
@@ -298,3 +426,14 @@ class Compile():
         cursor.execute(book_by_category)
         cursor.execute(book_handler_obj)
         cursor.execute(book_handler_obj_body)
+
+    def loan_handler_obj(cursor):
+        '''Compiles required functions and procedures for loan handler object.'''
+        cursor.execute(book_already_exists)
+        cursor.execute(is_loan)
+        cursor.execute(return_book_to_library)
+        cursor.execute(upd_deadline_date)
+        cursor.execute(get_loans_days)
+        cursor.execute(get_fine)
+        cursor.execute(loan_handler_obj)
+        cursor.execut(loan_handler_obj_body)
